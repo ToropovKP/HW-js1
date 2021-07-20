b0a3b2fddc40a9811388407ea84a6abd52cde292
ToropovKP <konstantin.toropov2002@yandex.ru>
2021-07-21 01:11:08 +0300

    to do list

--- todolist/app.js
+++ todolist/app.js
@@ -1,43 +1,61 @@
-let buttonEnter = document.getElementById('enter');
-let userInput = document.getElementById('userInput');
-let ul = document.querySelector('ul');
-
-function inputLength() {
-return userInput.value.length > 0;
-}
-
-function createTodo() {
-let li = document.createElement("li");
-li.appendChild(document.createTextNode(userInput.value));
-ul.appendChild(li);
-userInput.value = '';
-
-let deleteButton = document.createElement('button');
-deleteButton.appendChild(document.createTextNode('X'));
-li.appendChild(deleteButton);
-deleteButton.addEventListener('click', deleteTodoItem);
-
-li.addEventListener('click', function() {
-    li.classList.toggle('done');
+$(function() {
+    let buttonEnter = $('#enter');
+    let userInput = $('#userInput');
+    let ul = $('ul');
+    let localStorage= window.localStorage;
+    let todoMap=[
+        {
+            ind:1,
+            text:'example'
+        }
+    ]
+    
+    function inputLength() {
+        return !!userInput.val();
+    }
+    
+    function createTodo() {
+        let li = $("<li>");
+        li.fadeIn(150);
+        li.append(document.createTextNode(userInput.val()));
+        ul.append(li);
+        todoMap.push({
+            ind: todoMap.length *1,
+            text: userInput.val()
+        });
+        localStorage.setItem('Todo_list', JSON.stringify(todoMap));
+        userInput.val('');
+    
+        let deleteButton = $('<button>');
+        deleteButton.append(document.createTextNode('X'));
+        li.append(deleteButton);
+        deleteButton.click(deleteTodoItem);
+    
+        li.click(() => {
+           li.toggleClass('done');
+        });
+        
+        function deleteTodoItem() {
+            li.animate({
+                opacity: "0.4"
+            });
+            li.classList.add('delete');
+        }
+    }
+    
+    function changeListAfterKeypress(event) {
+        if (inputLength() && event.which === 13) {
+            createTodo();
+        }
+    }
+    
+    function changeAfterButtonPress() {
+        if (inputLength()) {
+            createTodo()
+        } 
+    }
+    
+    buttonEnter.click(changeAfterButtonPress);
+    userInput.keypress(changeListAfterKeypress);
+    
 })
-
-function deleteTodoItem() {
-li.classList.add('delete');
-}
-
-
-}
-
-function changeListAfterKeypress(event) {
-if (inputLength() && event.which === 13) {
-createTodo();
-}
-}
-
-function changeAfterButtonPress() {
-    if (inputLength()) {
-        createTodo()
-    } 
-}
-buttonEnter.addEventListener('click' , changeAfterButtonPress);
-userInput.addEventListener('keypress', changeListAfterKeypress);
